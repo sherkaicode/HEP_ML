8a0cf4e1873330acf490233070a4fe01c9bad7c2
sherkaicode <fljumawan@gmail.com>
2024-10-21 10:40:06 +0800

    Added the script to automate background launching

---
+++ Non-Resonant AD Extrapolation Repo/launch_100_times.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Path to your MadGraph process directory
+PROCESS_DIR="passport/non-res/madgraph_files/pp2jj_mc_test/"
+cd $PROCESS_DIR
+# Loop to run MadEvent 100 times
+for i in {1..100}
+do
+
+    ./bin/madevent launch -f
+
+    echo "Completed run_$i"
+done
